480c5e1e137cab57787c636d63623f029984af4e
botdrm <99725442+botdrm@users.noreply.github.com>
2022-02-20 09:52:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -437,7 +437,7 @@ async def account_login(bot: Client, m: Message):
             if raw_text0 in "anytimelearning" :
                 
                 y= url.replace("/", "%2F")
-#                 rout = f"https://live.anytimelearning.in/?route=common/ajax&mod=liveclasses&ack=getcustompolicysignedcookiecdn&stream=https%3A%2F%2Fvodcdn.anytimelearning.in%2Flivehttporigin%2F{y[56:-14]}%2Fmaster.m3u8"
+#                 rout = f"https://live.anytimelearning.in/?route=common/ajax&mod=liveclasses&ack=getcustompolicysignedcookiecdn&stream=https%3A%2F%2Fvodcdn.anytimelearning.in%2livehttporigin%2F{y[56:-14]}%2Fmaster.m3u8"
                 rout =f"https://live.anytimelearning.in/?route=common/ajax&mod=liveclasses&ack=getcustompolicysignedcookiecdn&stream=https%3A%2F%2Fvodcdn.anytimelearning.in%2F{y[39:-14]}%2Fmaster.m3u8"
                 getstatusoutput(f'curl "{rout}" -c "cookie.txt"')
                 cook = "cookie.txt"
